46d1ac2bfc47b9cc3cb5f9cc787179da608ff863
Apple <>
2026-09-25 06:56:38 +0900

    警告の修正

--- spec/requests/api/v1/articles_spec.rb
+++ spec/requests/api/v1/articles_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /api/v1/articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article, updated_at: 1.day.ago) }
-    let!(:article2) { create(:article, updated_at: 2.days.ago) }
-    let!(:article3) { create(:article, updated_at: Time.current) }
+    let!(:article_yesterday) { create(:article, updated_at: 1.day.ago) }
+    let!(:article_two_days_ago) { create(:article, updated_at: 2.days.ago) }
+    let!(:article_now) { create(:article, updated_at: Time.current) }
 
-    it "記事の一覧が取得できる" do
+    it "記事の一覧が取得できる", :aggregate_failures do
       subject
       res = JSON.parse(response.body)
 
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq(3)
-      expect(res.map {|d| d["id"] }).to eq([article3.id, article1.id, article2.id])
+      expect(res.map {|d| d["id"] }).to eq([article_now.id, article_yesterday.id, article_two_days_ago.id])
       expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
